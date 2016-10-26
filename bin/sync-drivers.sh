635046980228d30fa85523e73f8fa458ed399848
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2016-10-26 22:28:52 +0200

    Added the sync-drivers.

---
+++ bin/sync-drivers.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+GERMANIUM_DRIVERS_JAVA=$(readlink -f $(dirname $(readlink -f "$0"))/..)
+GERMANIUM_DRIVERS=$(readlink -f $(dirname $(readlink -f "$0"))/../../germaniumdrivers/)
+
+rm -fr $GERMANIUM_DRIVERS_JAVA/src/main/resources/binary
+cp -R $GERMANIUM_DRIVERS/germaniumdrivers/binary $GERMANIUM_DRIVERS_JAVA/src/main/resources/
