11489d015f95e6f8b0e962d1bcbe5a264196383c
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2016-10-26 22:37:27 +0200

    Copy the drivers correctly from upstream.

--- bin/sync-drivers.sh
+++ bin/sync-drivers.sh
@@ -4,4 +4,6 @@ GERMANIUM_DRIVERS_JAVA=$(readlink -f $(dirname $(readlink -f "$0"))/..)
 GERMANIUM_DRIVERS=$(readlink -f $(dirname $(readlink -f "$0"))/../../germaniumdrivers/)
 
 rm -fr $GERMANIUM_DRIVERS_JAVA/src/main/resources/binary
-cp -R $GERMANIUM_DRIVERS/germaniumdrivers/binary $GERMANIUM_DRIVERS_JAVA/src/main/resources/
+mkdir $GERMANIUM_DRIVERS_JAVA/src/main/resources/binary
+cp -R $GERMANIUM_DRIVERS/germaniumdrivers/binary/* $GERMANIUM_DRIVERS_JAVA/src/main/resources/binary
+
