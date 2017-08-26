0c34eb8998a9142ff633508f36c0765048298f04
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2017-08-26 23:35:05 +0200

    Deactivate the proxy.

--- jenkins/scripts/test-drivers.sh
+++ jenkins/scripts/test-drivers.sh
@@ -1,5 +1,29 @@
 #!/usr/bin/env bash
 
+# fail fast
+set -e
+
+#############################################################################
+# Utility functions.
+#############################################################################
+function deactivate_proxy() {
+    old_http_proxy="$http_proxy"
+    old_https_proxy="$https_proxy"
+    old_ftp_proxy="$ftp_proxy"
+    unset http_proxy
+    unset https_proxy
+    unset ftp_proxy
+}
+
+function activate_proxy() {
+    export http_proxy="$old_http_proxy"
+    export https_proxy="$old_https_proxy"
+    export ftp_proxy="$old_ftp_proxy"
+}
+
+deactivate_proxy
+
+# we checkout the sources of the project
 # we checkout the sources of the project
 git clone --recursive $SOURCES_URL /tmp/project
 
