6a6d545d4067f41bdd1ce558db95acbcd75bace9
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2017-08-26 23:24:51 +0200

    Checkout sources build in temp.

--- jenkins/scripts/test-drivers.sh
+++ jenkins/scripts/test-drivers.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+# we checkout the sources of the project
+git clone --recursive $SOURCES_URL /tmp/project
+
+source "/home/ciplogic/.sdkman/bin/sdkman-init.sh"
 cp /scripts/settings.xml /home/ciplogic/.m2/settings.xml
 
-cd /src
+cd /tmp/project
 mvn clean install
 
